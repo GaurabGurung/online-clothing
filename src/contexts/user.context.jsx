adcc07c9b03a7a17ec21eb0dd80e2dcba69dbb4f
Gaurab Gurung <gaurabgurung181@gmail.com>
2023-07-20 01:37:02 +1000

    Starting to add createContext method

---
+++ src/contexts/user.context.jsx
@@ -0,0 +1,16 @@
+import {createContext } from 'react';
+
+
+//as the actual value you want to access
+export const UserContext = createContext({
+    currentUser: null,
+    setCurrentUser : () => null,
+});
+
+
+export const UserProvider = ({ children }) => {
+    const [ currentUser, setCurrentUser ] = useState (null);
+    const value = { currentUser, setCurrentUser };
+
+    return <UserContext value= { value } > {children} </UserContext>
+};
